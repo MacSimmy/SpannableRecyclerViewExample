b8ba23cc580893b2ebf10a9ad66e800f50a24f75
MacSimmy <mschhimwalk@gmail.com>
2015-12-28 12:48:34 +0530

    Update MyRecyclerViewAdapter.java

--- app/src/main/java/chhimwal/mahendra/multipleviewrecyclerproject/MyRecyclerViewAdapter.java
+++ app/src/main/java/chhimwal/mahendra/multipleviewrecyclerproject/MyRecyclerViewAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.widget.CardView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -32,7 +33,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.textView.setText(mRViewDataList.get(position));
+        holder.bindDataWithViewHolder(mRViewDataList.get(position));
     }
 
     @Override
@@ -42,11 +43,32 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView textView;
+        private TextView textView;
+        private CardView cvItemView;
+        private String mDataItem=null;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            cvItemView=(CardView)itemView.findViewById(R.id.cv_item_view);
             textView = (TextView) itemView.findViewById(R.id.tvItemName);
+            cvItemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                  // One should handle onclick of event here based on the dataItem i.e. mDataItem in this case.
+                  // something like that..
+                /* Intent intent = new Intent(mContext,ResultActivity.class);
+                 intent.putExtra("MY_DATA",mDataItem);   //If you want to pass data.
+                 intent.putExtra("CLICKED_ITEM_POSTION",this.getAdapterPosition()); // If one want to get selected item position
+                 startActivity(intent);*/
+                }
+            });
+        }
+        
+        //This is clean method to bind data with viewHolder. Do all dirty things on View based on dataItem.
+        //Must be called from onBindViewHolder(),with dataItem. In our case dataItem is String object.
+        public void bindDataWithViewHolder(String dataItem){
+            this.mDataItem=dataItem;
+            textView.setText(mDataItem);
         }
     }
 }
